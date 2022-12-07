089567db4c210d1e8ea10be4254f5cb6e5242713
Samuel Ko <koshunyin@gmail.com>
2022-12-07 11:49:57 +1100

    add <head> tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import { store } from '../slices'
 import '../styles/global.css'
 import { BaseAppBar } from '../components/atoms/AppBar'
 import { BrandHeader } from '../components/atoms/BrandHeader'
+import Head from 'next/head'
 
 export default function App(props) {
 	const {
@@ -15,7 +16,9 @@ export default function App(props) {
 
 	return (
 		<ReduxProvider store={store}>
-			<title>{title}</title>
+			<Head>
+				<title>{title}</title>
+			</Head>
 			<CssBaseline />
 			<BaseAppBar>
 				<BrandHeader
