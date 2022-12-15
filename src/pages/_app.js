f91c7db7cca152e8ede4c03852beefd0e86ae726
Samuel Ko <koshunyin@gmail.com>
2022-12-15 11:56:15 +1100

    move some meta tags to _document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,15 +37,7 @@ export default function App(props) {
 					<IconContext.Provider value={{ color: theme.palette.primary.main }}>
 						<Head>
 							<title>Minesweeper</title>
-							<meta charSet='utf-8' />
 							<meta name='viewport' content='width=device-width, initial-scale=1' />
-							<meta name='description' content='The classic Minesweeper game built by Samuel Ko' />
-							<meta property='og:title' content='Minesweeper' />
-							<meta property='og:type' content='website' />
-							<meta property='og:url' content='https://minesweeper.samuelko123.com/' />
-							<meta property='og:image' content='https://minesweeper.samuelko123.com/og-image.png' />
-							<meta property='og:description' content='The classic Minesweeper game built by Samuel Ko' />
-							<link rel='manifest' href='site.webmanifest' />
 						</Head>
 						<CssBaseline />
 						<AppBar>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,16 @@ export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang='en'>
-				<Head />
+				<Head>
+					<meta charSet='utf-8' />
+					<meta name='description' content='The classic Minesweeper game built by Samuel Ko' />
+					<meta property='og:title' content='Minesweeper' />
+					<meta property='og:type' content='website' />
+					<meta property='og:url' content='https://minesweeper.samuelko123.com/' />
+					<meta property='og:image' content='https://minesweeper.samuelko123.com/og-image.png' />
+					<meta property='og:description' content='The classic Minesweeper game built by Samuel Ko' />
+					<link rel='manifest' href='site.webmanifest' />
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
