1b56111488bb8a9f5fa03afc16e8d3b2396395c1
Samuel Ko <koshunyin@gmail.com>
2022-12-15 11:23:38 +1100

    bug fix on og meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,31 +30,28 @@ export default function App(props) {
 		pageProps: { ...pageProps },
 	} = props
 
-	const title = 'Minesweeper'
-	const desc = 'The classic Minesweeper game built by Samuel Ko'
-
 	return (
 		<ReduxProvider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
 				<ThemeProvider theme={theme}>
 					<IconContext.Provider value={{ color: theme.palette.primary.main }}>
 						<Head>
-							<title>{title}</title>
+							<title>Minesweeper</title>
 							<meta charSet='utf-8' />
 							<meta name='viewport' content='width=device-width, initial-scale=1' />
-							<meta name='description' content={desc} />
+							<meta name='description' content='The classic Minesweeper game built by Samuel Ko' />
 							<meta property='og:title' content='Minesweeper' />
 							<meta property='og:type' content='website' />
-							<meta property='og:url' content={process.env.NEXT_PUBLIC_URL} />
-							<meta property='og:image' content={process.env.NEXT_PUBLIC_URL + '/og-image.png'} />
-							<meta property='og:description' content={desc} />
+							<meta property='og:url' content='https://minesweeper.samuelko123.com/' />
+							<meta property='og:image' content='https://minesweeper.samuelko123.com/og-image.png' />
+							<meta property='og:description' content='The classic Minesweeper game built by Samuel Ko' />
 							<link rel='manifest' href='site.webmanifest' />
 						</Head>
 						<CssBaseline />
 						<AppBar>
 							<BrandHeader
 								href='/'
-								title={title}
+								title='Minesweeper'
 							/>
 							<MenuButton
 								size={32}
